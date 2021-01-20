56043c05aaf97102135241a738fbd611d992f9e4
jlbuild <juliabuildbot@gmail.com>
2021-01-20 13:39:05 +0000

    AMGX_jll build 2.1.0+0

---
+++ src/AMGX_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule AMGX_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("AMGX")
+JLLWrappers.@generate_main_file("AMGX", UUID("656d14af-56e4-5275-8e68-4e861d7b5043"))
+end  # module AMGX_jll
